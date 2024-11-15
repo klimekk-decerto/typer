da93c7141d31d3b67d4b48659c5a3c9f24d6d70e
Klimek Kamil <kamil.klimek@decerto.com>
2024-11-15 19:57:02 +0100

    Initial backend spring boot

---
+++ backend/src/test/java/com/decerto/typer/TyperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decerto.typer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
