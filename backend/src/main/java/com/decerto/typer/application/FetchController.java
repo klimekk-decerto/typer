7f7c6c29b5da44a2fcfa9582fc2c80da121a3355
szwolinski <szymon.zwolinski95@gmail.com>
2024-11-16 06:23:18 +0100

    list of competitions

--- backend/src/main/java/com/decerto/typer/application/FetchController.java
+++ backend/src/main/java/com/decerto/typer/application/FetchController.java
@@ -1,5 +1,6 @@
 package com.decerto.typer.application;
 
+import com.decerto.typer.CompetitionDetails;
 import com.decerto.typer.competition.CompetitionRepository;
 import com.decerto.typer.CompetitionDto;
 import com.decerto.typer.competition.CompetitionEntity;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/fetch")
@@ -23,8 +26,16 @@ public class FetchController {
     @GetMapping("/competition/{id}")
     public CompetitionDto getCompetition(@PathVariable long id) {
         CompetitionEntity competition = competitionRepository.findById(id).orElseThrow();
-        ScheduleEntity schedule = scheduleRepository.findById(id).orElseThrow();
+        ScheduleEntity schedule = scheduleRepository.findByCompetitionId(id);
         return competitionMapper.map(competition, schedule);
+    }
+
+    @GetMapping("/competitions")
+    public List<CompetitionDetails> getCompetitions() {
+        return competitionRepository.findAll()
+                .stream()
+                .map(entity -> new CompetitionDetails(entity.getId(), entity.getName()))
+                .toList();
     }
 
 }
