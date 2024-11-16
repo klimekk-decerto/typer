aeab41f58f0bc1fdce2e476c802fdd38104032a0
szwolinski <szymon.zwolinski95@gmail.com>
2024-11-16 07:43:53 +0100

    list of competitions - new page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from "./core/auth/login.component";
 import {BlankComponent} from "./core/layout/blank/blank.component";
 import {MainPageComponent} from "./core/admin/main-page/main-page.component";
+import {CompetitionListComponent} from "./core/admin/start/competition-list.component";
 
 export const routes: Routes = [
   // {path: '', component: BlankComponent},
@@ -15,6 +16,10 @@ export const routes: Routes = [
       {
         path: 'admin-dashboard',
         component: MainPageComponent
+      },
+      {
+        path: 'admin-dashboard-competitions',
+        component: CompetitionListComponent
       },
     ],
   }
